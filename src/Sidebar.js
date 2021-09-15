262622688add4389cb9ea4055473eb4e46275f4d
SajawalHassan <SajawalHassan>
2021-09-15 22:13:24 +0500

    Added sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Sidebar.css";
+
+import SidebarOptions from "./SidebarOptions";
+import VideocamIcon from "@material-ui/icons/Videocam";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <VideocamIcon />
+
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+      <SidebarOptions img="https://static-cdn.jtvnw.net/jtv_user_pictures/811dc7d4-1079-4ead-8b48-97f3ff4fa361-profile_image-70x70.png" />
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/SidebarOptions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SidebarOptions.css";
+
+function SidebarOptions({ img }) {
+  return (
+    <div className="sidebarOptions">
+      <img src={img} alt="" />
+    </div>
+  );
+}
+
+export default SidebarOptions;
